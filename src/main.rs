44208807420d2e46202f0ae12bb727f50f6dbdbc
Berghopper <12167073+Berghopper@users.noreply.github.com>
2024-09-14 19:02:00 +0300

    C bindings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+// Declare externals
+extern "C" {
+    fn init_360_gadget(await_endpoint_availability: bool) -> i32;
+    fn close_360_gadget();
+    fn send_to_ep1(fd: i32, data: *const u8, len: usize) -> bool;
+    fn gadget_example();
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Call the C function
+    println!("Calling init_360_gadget...");
+    unsafe { gadget_example() };
 }
